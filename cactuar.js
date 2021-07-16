f0cfc30af709ed897fad2564dfa2f85334785e45
Kerrance <hello@kerrisharp.com>
2021-07-16 14:12:22 +0100

    Include functionality for expanding question and response pairs

--- cactuar.js
+++ cactuar.js
@@ -6,8 +6,24 @@
  * Website: https://kerrance.github.io/cactuar/
  */
 ( function() {
+  const questionTitle = document.getElementsByClassName( 'cactuar__title' );
+  let i;
+
+  function expandQuestionResponse() {
+    this.classList.toggle( 'cactuar__title--active' );
+    let questionResponse = this.nextElementSibling;
+
+    if ( questionResponse.style.maxHeight ){
+      questionResponse.style.maxHeight = null;
+    } else {
+      questionResponse.style.maxHeight = questionResponse.scrollHeight + 'px';
+    }
+  }
+
   function eventListener() {
-    //
+    for ( i = 0; i < questionTitle.length; i++ ) {
+      questionTitle[i].addEventListener( 'click', expandQuestionResponse );
+    }
   }
 
   function initCactaur() {
