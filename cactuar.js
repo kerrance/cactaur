5ace738d0782bb6767a0a89f085e5462dcd4661e
Kerrance <hello@kerrisharp.com>
2021-07-16 16:33:05 +0100

    Implement response pair filtering

--- cactuar.js
+++ cactuar.js
@@ -7,8 +7,12 @@
  */
 ( function() {
   const contexts = document.getElementsByClassName( 'cactuar__context' );
+  const responsePairs = document.getElementsByClassName( 'cactuar__pair' );
   const questionTitle = document.getElementsByClassName( 'cactuar__title' );
+  let matchedPairs
   let contextsInt;
+  let selectedContextInt;
+  let matchedPairsInt;
   let questionPairInt;
 
   function expandQuestionResponse() {
@@ -28,6 +32,24 @@
     this.className += " cactuar__context--active";
   }
 
+  function filterSelectedContext() {
+    let selectedContext = document.getElementsByClassName( 'cactuar__context--active' );
+    let selectedContextTarget = selectedContext[0].getAttribute('data-target');
+    if ( selectedContextTarget === 'cactuar__pair--all' ) selectedContextTarget = '';
+
+    for ( selectedContextInt = 0; selectedContextInt < responsePairs.length; selectedContextInt++ ) {
+      matchedPairs = responsePairs[selectedContextInt];
+
+      for ( matchedPairsInt = 0; matchedPairsInt < matchedPairs.classList.length; matchedPairsInt++ ) {
+        if ( matchedPairs.classList[matchedPairsInt].match( selectedContextTarget ) ) {
+          matchedPairs.style.display = 'block';
+        } else {
+          matchedPairs.style.display = 'none';
+        }
+      }
+    }
+  }
+
   function eventListener() {
     for ( questionPairInt = 0; questionPairInt < questionTitle.length; questionPairInt++ ) {
       questionTitle[questionPairInt].addEventListener( 'click', expandQuestionResponse );
@@ -35,6 +57,7 @@
 
     for ( contextsInt = 0; contextsInt < contexts.length; contextsInt++ ) {
       contexts[contextsInt].addEventListener( 'click', selectActiveContext );
+      contexts[contextsInt].addEventListener( 'click', filterSelectedContext );
     }
   }
 
